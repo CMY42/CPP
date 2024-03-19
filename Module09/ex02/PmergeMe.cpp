fc39a730bbe1fafcc2a1647122d255aca511fb41
cmy <116715067+cmansey@users.noreply.github.com>
2024-03-19 15:11:39 +0100

    refonte ex02 mod09 fusion/insertion

--- Module09/ex02/PmergeMe.cpp
+++ Module09/ex02/PmergeMe.cpp
@@ -22,64 +22,189 @@ void PmergeMe::addData(int number)
 	dataList.push_back(number);
 }
 
-void PmergeMe::displaySequence(const std::string& phase) const
+void PmergeMe::displaySequence(const std::string& phase, const std::vector<int>& data) const
 {
-	std::cout << phase << ": ";
-	if (phase == "Before")
-	{
-		for (int num : dataVector)
-		{
-			std::cout << num << " ";
-		}
-	}
-	else if (phase == "After")
-	{
-		for (int num : dataList)
-		{
-			std::cout << num << " ";
-		}
+	for (std::vector<int>::const_iterator it = data.begin(); it != data.end(); ++it) {
+		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
+}
+
+void PmergeMe::displaySequence(const std::string& phase, const std::list<int>& data) const
+{
+	for (std::list<int>::const_iterator it = data.begin(); it != data.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::insertionSortList(std::list<int>& list)
+{
+	if (list.size() <= 1) return; // Aucun tri nécessaire pour une liste de 0 ou 1 élément.
+
+	std::list<int>::iterator it1 = list.begin();
+	++it1; // Commencez à partir du deuxième élément
+
+	while (it1 != list.end()) {
+		int val = *it1; // Sauvegardez la valeur actuelle à insérer
+		std::list<int>::iterator it2 = it1;
+		std::list<int>::iterator place;
+
+		// Trouvez l'emplacement d'insertion approprié pour la valeur actuelle
+		bool found = false;
+		for (place = list.begin(); place != it1; ++place) {
+			if (*place > val)
+			{
+				found = true;
+				break;
+			}
+		}
+
+		if (found)
+		{
+			// Si un emplacement d'insertion est trouvé avant it1, insérez la valeur et effacez l'ancien élément
+			list.insert(place, val); // Insère la valeur avant 'place'
+			it1 = list.erase(it1); // Efface l'élément à 'it1', retourne l'itérateur au suivant
+		}
+		else
+		{
+			++it1; // Continuez à l'élément suivant si aucun emplacement n'est trouvé
+		}
+	}
+}
+
+std::list<int> PmergeMe::mergeLists(std::list<int>& left, std::list<int>& right)
+{
+	std::list<int> result;
+	while (!left.empty() && !right.empty())
+	{
+		if (left.front() <= right.front())
+		{
+			result.push_back(left.front());
+			left.pop_front();
+		}
+		else
+		{
+			result.push_back(right.front());
+			right.pop_front();
+		}
+	}
+
+	// Ajoutez les éléments restants
+	while (!left.empty())
+	{
+		result.push_back(left.front());
+		left.pop_front();
+	}
+
+	while (!right.empty())
+	{
+		result.push_back(right.front());
+		right.pop_front();
+	}
+
+	return result;
+}
+
+void PmergeMe::mergeSortList(std::list<int>& list, int threshold)
+{
+	if (list.size() <= 1) return;
+
+	// Divisez la liste en deux moitiés
+	std::list<int> left;
+	std::list<int> right;
+	std::list<int>::iterator middle = list.begin();
+	std::advance(middle, list.size() / 2);
+
+	left.splice(left.begin(), list, list.begin(), middle);
+	right.splice(right.begin(), list, middle, list.end());
+
+	// Tri fusion récursif sur les moitiés
+	if (left.size() > threshold) mergeSortList(left, threshold);
+	else insertionSortList(left);
+	if (right.size() > threshold) mergeSortList(right, threshold);
+	else insertionSortList(right);
+
+	// Fusionner les moitiés triées
+	list = mergeLists(left, right);
+}
+
+void PmergeMe::fordJohnsonSortList(std::list<int>& list)
+{
+	int threshold = 16; // Seuil pour passer du tri fusion au tri par insertion, ajustez selon les besoins
+	mergeSortList(list, threshold);
+}
+
+void PmergeMe::insertionSort(std::vector<int>& data, int left, int right)
+{
+	for (int i = left + 1; i <= right; ++i)
+	{
+		int key = data[i];
+		int j = i - 1;
+		while (j >= left && data[j] > key)
+		{
+			data[j + 1] = data[j];
+			j = j - 1;
+		}
+		data[j + 1] = key;
+	}
+}
+
+void PmergeMe::mergeSort(std::vector<int>& data, int left, int right, int threshold)
+{
+	if (left < right)
+	{
+		if (right - left <= threshold)
+		{ // Utilisez le tri par insertion pour de petites sous-listes
+			insertionSort(data, left, right);
+		}
+		else
+		{
+			int mid = left + (right - left) / 2;
+			mergeSort(data, left, mid, threshold);
+			mergeSort(data, mid + 1, right, threshold);
+			std::inplace_merge(data.begin() + left, data.begin() + mid + 1, data.begin() + right + 1);
+		}
+	}
+}
+
+void PmergeMe::fordJohnsonSort(std::vector<int>& data)
+{
+	int threshold = 16; // Seuil pour passer du tri fusion au tri par insertion, ajustez selon les besoins
+	mergeSort(data, 0, data.size() - 1, threshold);
+}
+
+void PmergeMe::simpleSort(std::list<int>& data)
+{
+	data.sort(); // Utilisation de la méthode sort de std::list
 }
 
 
 void PmergeMe::sortAndDisplay()
 {
 	// Affichage avant le tri
-	std::cout << "\033[31m" << "Before:" << "\033[0m" << " ";
-	for (int num : dataVector)
-	{
-		std::cout << num << " ";
-	}
-	std::cout << std::endl;
+	std::cout << "\033[31mBefore:\033[0m ";
+	displaySequence("Before", dataVector);
 
-	// Mesure du temps pour std::vector
+	// Mesure du temps pour std::vector avec Ford-Johnson
 	clock_t startVector = clock();
-	std::sort(dataVector.begin(), dataVector.end());
+	fordJohnsonSort(dataVector); //tri du std::vector
 	clock_t endVector = clock();
 
 	// Affichage après le tri pour std::vector
-	std::cout << "\033[34m" << "After (std::vector): " << "\033[0m";
-	for (int num : dataVector)
-	{
-		std::cout << num << " ";
-	}
-	std::cout << std::endl;
-	std::cout << "\033[34m" << "Time to process with std::vector: " << "\033[0m" << (double)(endVector - startVector) / CLOCKS_PER_SEC * 1000000 << " us\n";
+	std::cout << "\033[34mAfter (std::vector):\033[0m ";
+	displaySequence("After", dataVector);
+	std::cout << "\033[34mTime to process with std::vector:\033[0m " << (double)(endVector - startVector) / CLOCKS_PER_SEC * 1000000 << " us\n";
 
-	// Mesure du temps pour std::list
+	// Mesure du temps pour std::list avec Ford-Johnson
 	clock_t startList = clock();
-	dataList.sort();
+	fordJohnsonSortList(dataList); // Tri du std::list
 	clock_t endList = clock();
 
-	// Affichage après le tri pour std::list
-	std::cout << "\033[33m" << "After (std::list): " << "\033[0m";
-	for (int num : dataList)
-	{
-		std::cout << num << " ";
-	}
-	std::cout << std::endl;
-	std::cout << "\033[33m" << "Time to process with std::list: " << "\033[0m" << (double)(endList - startList) / CLOCKS_PER_SEC * 1000000 << " us\n";
+	std::cout << "\033[33mAfter (std::list):\033[0m ";
+	displaySequence("After", dataList);
+	std::cout << "\033[33mTime to process with std::list:\033[0m " << (double)(endList - startList) / CLOCKS_PER_SEC * 1000000 << " us\n";
 }
+
 
 
--- Module09/ex02/PmergeMe.hpp
+++ Module09/ex02/PmergeMe.hpp
@@ -3,34 +3,36 @@
 
 #include <iostream> // pour std::cout
 #include <vector> // container pour tableau dynamique
-#include <algorithm> // pour std::sort
+#include <algorithm> // pour std::sort et std::merge
 #include <list> // container pour liste doublement chaînée
 #include <ctime> // pour clock_t
+#include <iterator> // pour std::distance
 
-/*class PmergeMe
-{
-public:
-	PmergeMe();
-	PmergeMe(const PmergeMe &other);
-	PmergeMe &operator=(const PmergeMe &other);
-	virtual ~PmergeMe();
-
-	void sortAndMeasure(const std::vector<int> &inputSequence);
-
-private:
-		void mergeSort(std::vector<int> &sequence);
-		void sortList(std::list<int> &sequence);
-		template <typename T>
-		void displaySequence(const T &sequence) const;
-		template <typename Container> //permet d'utiliser la fonction avec un vector ou une list, deux containers différents sans avoir à écrire deux fonctions différentes
-		void measureSortingTime(Container &sequence);
-};*/
-
-
-
-// std::vector: Le tri est généralement plus rapide sur les vecteurs en raison de l'accès contigu à la mémoire, ce qui améliore la localité de cache et réduit le temps d'accès aux éléments lors du tri. L'algorithme de tri standard (std::sort) est très optimisé pour des structures de données comme le vecteur.
-// std::list: Bien que std::list::sort() soit optimisé pour les listes chaînées, le processus de tri peut être plus lent que pour un vecteur car il n'y a pas d'accès direct aux éléments intermédiaires. Chaque déplacement à travers la liste nécessite de suivre les pointeurs depuis l'élément actuel jusqu'à l'élément cible, ce qui peut augmenter le temps d'exécution comparé à un vecteur.
+// std::vector: Le tri est généralement plus rapide sur les vecteurs en raison de l'accès contigu à la mémoire,
+// ce qui améliore la localité de cache et réduit le temps d'accès aux éléments lors du tri.
+// L'algorithme de tri standard (std::sort) est très optimisé pour des structures de données comme le vecteur.
+// std::list: Bien que std::list::sort() soit optimisé pour les listes chaînées,
+// le processus de tri peut être plus lent que pour un vecteur car il n'y a pas d'accès direct aux éléments intermédiaires.
+// Chaque déplacement à travers la liste nécessite de suivre les pointeurs depuis l'élément actuel jusqu'à l'élément cible,
+// ce qui peut augmenter le temps d'exécution comparé à un vecteur.
 // La différence de temps d'exécution sera donc principalement due à la manière dont la mémoire est accédée et manipulée dans chaque type de conteneur
+
+// L'algorithme Ford-Johnson est une méthode de tri intéressante du point de vue de la théorie algorithmique,
+// surtout pour son approche visant à réduire le nombre de comparaisons nécessaires.
+// Cependant, sa complexité et ses performances variables en fonction du contexte d'utilisation limitent son application pratique
+// par rapport à d'autres algorithmes de tri plus courants et plus facilement implémentables
+// Diviser pour régner : L'algorithme divise la liste à trier en petits groupes.
+// Idéalement, ces groupes sont de taille 2 (mais cela peut varier selon l'implémentation et la taille de la liste).
+// Trouver les médianes : Pour chaque groupe, trouvez la médiane en les comparant.
+// Pour un groupe de deux, c'est simplement en comparant quel élément est plus grand. Les médianes servent de points de référence pour les étapes suivantes.
+// Diviser selon la médiane des médianes : Une fois que vous avez une liste de médianes pour chaque groupe,
+// trouvez la médiane de ces médianes. Cette médiane des médianes est utilisée pour diviser la liste originale en deux parties :
+// une partie avec des éléments inférieurs à la médiane des médianes et l'autre avec des éléments supérieurs.
+// Tri récursif : Appliquez récursivement l'algorithme aux deux moitiés de la liste,
+// en utilisant la même méthode pour diviser chaque moitié en groupes, trouver les médianes, et ainsi de suite.
+// Fusion et insertion : Une fois que vous avez atteint des listes de taille gérable
+// (souvent quand elles sont réduites à un ou deux éléments), vous les fusionnez en utilisant une méthode d'insertion,
+// en plaçant chaque élément à sa position correcte par rapport aux autres dans la liste finale triée.
 
 class PmergeMe
 {
@@ -50,8 +52,25 @@ public:
 	// Méthode pour trier et afficher les séquences
 	void sortAndDisplay();
 
-	// Méthode pour afficher une séquence
-	void displaySequence(const std::string& phase) const;
+	void displaySequence(const std::string& phase, const std::vector<int>& data) const;
+
+	void displaySequence(const std::string& phase, const std::list<int>& data) const;
+
+	void fordJohnsonSortList(std::list<int>& list);
+
+	void insertionSortList(std::list<int>& data);
+
+	std::list<int> mergeLists(std::list<int>& left, std::list<int>& right);
+
+	void mergeSortList(std::list<int>& list, int threshold);
+
+	void fordJohnsonSort(std::vector<int>& data);
+
+	void insertionSort(std::vector<int>& data, int left, int right);
+
+	void mergeSort(std::vector<int>& data, int left, int middle, int right);
+
+	void simpleSort(std::list<int>& data);
 };
 
 #endif
